97ac61d16586e603dd20e1c23f081a8518ef06f2
Arckangel <benjamin.caroux@gmail.com>
2016-11-24 12:03:04 +0100

    Premiere modification

---
+++ js/javascript.js
@@ -0,0 +1,48 @@
+
+
+function Tester(){
+  var mot = document.getElementById("reponse").value;
+  var gra = document.getElementById("mot1");
+  var min = document.getElementById("mot2");
+  var maj = document.getElementById("mot3");
+  var nombre = document.getElementById("nbcv");
+  var inverse = document.getElementById("reverse");
+  var palindrome = document.getElementById("palin");
+
+  // --------Afficher le mot en gras et en couleur---------------- //
+  gra.innerHTML = mot;
+  gra.style.color = "red";
+  gra.style.fontWeight = "bold";
+
+  // --------Afficher le mot en minuscules------------------------ //
+  min.innerHTML = mot.toLowerCase();
+
+  // --------Afficher le mot en majuscules------------------------ //
+  maj.innerHTML = mot.toUpperCase();
+
+  // --------Afficher le nombre de voyelles et de consonnes------- //
+  var lettres = mot.toLowerCase().split('');
+  var voy = 0;
+  for (var i = 0; i < lettres.length; i++) {
+    if (lettres[i]=="a"||lettres[i]=="e"||lettres[i]=="i"||lettres[i]=="o"||lettres[i]=="u"||lettres[i]=="y"){
+      voy = voy+1;
+    }
+  }
+  var cons = lettres.length - voy;
+  nombre.innerHTML = "Le mot contient " + voy + " voyelles et " + cons + " consonnes";
+
+  // --------Afficher le mot à l'envers--------------------------- //
+  var inv = "";
+  for (var j = mot.length-1; j >= 0; j--) {
+    inv+=mot[j];
+  }
+  inverse.innerHTML = "Le mot à l'envers est '" + inv + "'";
+
+  // --------Afficher s'il s'agit d'un palindrome ou non---------- //
+  if(inv.toLowerCase()==mot.toLowerCase()){
+    palindrome.innerHTML = mot + " est un palindrome";
+  }
+  else{
+    palindrome.innerHTML = mot + " n'est pas un palindrome";
+  }
+}
